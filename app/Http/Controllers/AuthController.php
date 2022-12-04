f60300061e73d1a9f98408bdf8dc42d237fa072b
sychadelite <hashiramakun58@gmail.com>
2022-12-04 22:40:26 +0700

    sign up api

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,291 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\Auth;
+
+use App\Models\User;
+
+
+class AuthController extends Controller
+{
+    public function IndexUsers() {
+        $users = DB::table('users')->get();
+
+        return response()->json([
+            'status' => true,
+            'message' => 'got all of the available users',
+            'data' => $users
+        ]);
+    }
+
+    public function IndexAuthUser() {
+        $auth_user = DB::table('users')->where('id', auth()->user()->id)->first();
+        return response()->json([
+            'status' => true,
+            'message' => 'got your authenticated information',
+            'data' => $auth_user
+        ]);
+    }
+
+    public function IndexUserByName($name){
+        $auth_user = DB::table('users')->where('name', $name)->first();
+
+        return response()->json([
+            'status' => true,
+            'message' => 'got your authenticated information',
+            'data' => $auth_user
+        ]);
+    }
+
+    public function indexAllEmail(Request $request) {
+
+        $validation = Validator::make($request->all(), [                                                      
+            'email' => 'required|email|unique:users',
+        ]);
+        
+        if ($validation->fails()) {            
+            return response()->json([
+                "status" => true,
+                "message" => $validation->errors(),
+                "data" => "error",
+            ], 200);
+        }
+
+        return response()->json([
+            "status" => true,
+            "message" => 'Email is Unique',
+            "otp" => str_pad((random_int(0, 999999)), 6, 0, STR_PAD_LEFT)
+        ]);
+    }
+
+    public function indexReferrerCode($referral) {
+        $fetch_referrer = DB::table('users')
+            ->select('users.*')
+            ->where('users.referral', $referral)
+            ->first();
+
+        if($fetch_referrer) {
+            return response()->json([
+                "status" => true,
+                "message" => '*Data Referrer Code Collected',
+                "data" => $fetch_referrer
+            ]);
+        } else {
+            return response()->json([
+                "status" => false,
+                "message" => '*Invalid Referrer Code'
+            ], 403);
+        }
+    }
+
+    public function storeUser(Request $request) {
+
+        $validation = Validator::make($request->all(), [
+            'name' => 'required|unique:users|min:4|max:12',                                                      
+            'email' => 'required|email|unique:users',
+            'password' => 'required|min:4|max:12|required_with:repassword|same:repassword',
+            'repassword' => 'min:4|max:12|'
+        ]);
+
+        if ($validation->fails()) :
+            return response()->json([
+                "status" => false,
+                "message" => $validation->errors(),
+            ], 403);
+        endif;
+
+        $referrer = User::where('referral', $request['referrer'])->first();
+
+        if(!$referrer) {
+            if ($request['referrer'] == null) {
+                $insertUser = User::create([
+                    "name" => StrToLower(Trim($request['name'])),
+                    "email" => StrToLower(Trim($request['email'])),
+                    "password" => Hash::make($request['password']),
+                    "referral" => $this->generateUniqueCode(),
+                    "referrer" => $request['referrer']                    
+                ]);
+                
+                $user = User::where('name', StrToLower(Trim($request['name'])))->first();
+                $token = $user->createToken('itempedia_token')->plainTextToken;
+
+                $res = [
+                    'user' => $insertUser,
+                    'access_token' => $token
+                ];
+        
+                if ($res) :
+                    return response()->json([
+                        "status" => true,
+                        "message" => 'Signup Completed',
+                        "data" => $res
+                    ]);
+                endif;
+            }
+
+            return response([
+                'message' => 'Referral Code not found'
+            ], 406);
+            
+        } else {
+            $insertUser = User::create([
+                "name" => StrToLower(Trim($request['name'])),
+                "email" => StrToLower(Trim($request['email'])),
+                "password" => Hash::make($request['password']),
+                "referral" => $this->generateUniqueCode(),
+                "referrer" => $request['referrer']                    
+            ]);
+
+            $user = User::where('name', StrToLower(Trim($request['name'])))->first();
+            $token = $user->createToken('itempedia_token')->plainTextToken;
+
+            $res = [
+                'user' => $insertUser,
+                'access_token' => $token
+            ];
+    
+            if ($res) :
+                return response()->json([
+                    "status" => true,
+                    "message" => 'Signup Completed',
+                    "data" => $res
+                ]);
+            endif;
+        }
+    }
+
+
+
+    public function generateUniqueCode() {
+
+        $characters = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+        $charactersNumber = strlen($characters);
+        $codeLength = 6;
+
+        $code = '';
+
+        while (strlen($code) < 6) {
+            $position = rand(0, $charactersNumber - 1);
+            $character = $characters[$position];
+            $code = $code.$character;        
+        }
+
+        if (User::where('referral', $code)->exists()) {
+            $this->generateUniqueCode();
+        }
+
+        return $code;
+    }
+
+
+
+    // =============================================================== Sign In ================================================================================
+
+
+    public function signInUser(Request $request)
+    {
+        // check email
+        $user = User::where('name', $request['name'])->first();
+
+        // check password
+        if(!$user || !Hash::check($request['password'], $user->password2)) {
+            return response([
+                'message' => 'Bad Credentials'
+            ], 401);
+        }
+
+        $token = $user->createToken('wrathnet_token')->plainTextToken;
+
+        return response()->json([
+            "status" => true,
+            "message" => 'Hi '.$user->name.', welcome to Itempedia',
+            "data" => $user,
+            "access_token" => $token,
+            "token_type" => 'Bearer'
+        ]);
+    }
+
+    public function getUser()
+    {
+        // return auth()->user();
+        $token = request()->user()->currentAccessToken()->token;
+
+        $userAuth = auth()->user();
+
+        $path = public_path('assets/avatar'); 
+        
+        $avaPath = '/assets/avatar/';
+        
+        // check user avatar availability
+        if(!empty($userAuth->avatar) && file_exists($path.'/'.$userAuth->avatar)) {       // validation if user has avatar data
+            // $token = $request->bearerToken();
+            $userAuth->avatar = $avaPath . $userAuth->avatar;
+            
+            return response()->json([
+                "status" => true,
+                "message" => 'Data collected',
+                "access_token" => $token,
+                'token_type' => 'Bearer',
+                "data" => $userAuth
+            ]);
+        } else {            
+            $userAuth->avatar = $avaPath . "default.png";
+
+            return response()->json([
+                "status" => true,
+                "message" => 'Data collected',
+                "access_token" => $token,
+                'token_type' => 'Bearer',
+                "data" => $userAuth
+            ]);            
+        }        
+    }
+
+    public function destroyTokens() {
+        $key = [];
+        
+        $tokens_count = DB::table('personal_access_tokens')
+        ->count();
+
+        $deleteUs = DB::table('personal_access_tokens')
+        ->where('tokenable_id', '=', auth()->user()->id)
+        ->latest()
+        ->take($tokens_count)
+        ->skip(3)
+        ->get();
+
+        foreach($deleteUs as $deleteMe) {
+            $key[] = $deleteMe->id;
+        }
+        
+        $asd = DB::table('personal_access_tokens')
+        ->delete($key);
+
+        if ($asd) {
+            return response()->json([
+                'status' => true,
+                'message' => 'Other tokens is deleted except 3 newest'
+            ]);
+        }
+    }
+
+
+    // =============================================================== Sign Out ================================================================================
+
+
+    public function signOutUser()
+    {
+        // auth()->user()->tokens()->delete();                                  // Revoke all of tokens based on logged ID
+        auth()->user()->currentAccessToken()->delete();                         // Revoke current token based on logged ID
+
+        return response()->json([
+            "status" => true,
+            "message" => 'You have successfully logged out',
+        ]);
+    }
+}
---
+++ routes/api.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+use App\Http\Controllers\AuthController;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+// PROTECTED ROUTES
+Route::group(['middleware' => ['auth:sanctum']], function() {
+    Route::get('/auth/user', [AuthController::class, 'IndexAuthUser']);
+    Route::get('/auth/user/detail', [AuthController::class, 'getUser']);
+});
+
+// PUBLIC ROUTES
+Route::get('/users', [AuthController::class, 'IndexUsers']);
+Route::get('/users/{custom}', [AuthController::class, 'IndexUserByName']);
+
+
+Route::post('/users/checkemail', [AuthController::class, 'indexAllEmail']);
+Route::get('/users/checkreferral/{code}', [AuthController::class, 'indexReferrerCode']);
+Route::post('/users/signup', [AuthController::class, 'storeUser']);
+Route::post('/users/signin', [AuthController::class, 'signInUser']);
