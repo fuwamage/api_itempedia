1bb50a43ddce5d1684666f99d2b4e8d424f9efd7
sychadelite <hashiramakun58@gmail.com>
2022-12-18 03:37:16 +0700

    add credential

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,6 +19,8 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('avatar')->nullable();
+            $table->string('referral')->nullable();
+            $table->string('referrer')->nullable();
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
